9e3f6122cb9658b61daca51e228dbfcabef27408
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-03-03 16:42:58 +1100

    forced user to signin while visiting profile, forced user to edit profile when profile information is not stored

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,13 @@
 class ProfilesController < ApplicationController
+    before_action :authenticate_user!
     before_action :set_profile
 
     # GET /myprofile
     def show
+      # when the user have not updated profile redirect to update
+      if !current_user.profile.name
+        redirect_to edit_myprofile_path, alert: 'Please update profile!'
+      end
     end
 
     # GET /myprofile/public
@@ -15,19 +20,17 @@ class ProfilesController < ApplicationController
   
     # PATCH/PUT /myprofile
     def update
-      respond_to do |format|
-        if @profile.save(profile_params)
-            redirect_to @profile, notice: 'Profile was successfully saved.'
-        else
-            render :edit
-        end
+      if @profile.update(profile_params)
+        redirect_to myprofile_path, notice: 'Profile was successfully saved.'
+      else
+        render :edit
       end
     end
 
     private
       # To retrieve current user profile or create a new if there is none.
       def set_profile
-        @profile = current_user.profile || Profile.new(user_id: current_user.id)
+        @profile = current_user.profile || current_user.create_profile
       end
   
       # Only allow name, phone, location and photo
