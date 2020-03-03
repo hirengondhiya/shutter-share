64fb3a303ab999d592148e72bdcf362187828d62
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-03-03 14:42:39 +1100

    added routes for profile management

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
+
+  get '/myprofile', to: 'profiles#show', as: 'myprofile'
+  post '/myprofile', to: 'profiles#update'
+  get '/myprofile/public', to: 'profiles#public', as: 'myprofile_public'
+  get '/myprofile/edit', to: 'profiles#edit', as: 'edit_myprofile'
 end
