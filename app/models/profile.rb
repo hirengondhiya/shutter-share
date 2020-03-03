5dcbca8ffeb05e672d661f5a53ed5fd09fd92525
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-03-03 13:23:49 +1100

    code comment on phone number validation

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,6 +3,9 @@ class Profile < ApplicationRecord
 
   validates :name, presence: true
   validates :location, presence: true
+
+  # regular expression to check valid australian phone numbers
+  # The regex does not match with empty string hence no need for presence validation
   validates :phone,
             format: {
               with: /\A\({0,1}((0|\+61)(2|4|3|7|8)){0,1}\){0,1}(\ |-){0,1}[0-9]{2}(\ |-){0,1}[0-9]{2}(\ |-){0,1}[0-9]{1}(\ |-){0,1}[0-9]{3}\z/,
