f699cb2de746fb98cfccd850b3ca9d3dfc06a5db
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-03-04 10:55:45 +1100

    added picture field to profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,7 +3,6 @@ class Profile < ApplicationRecord
 
   validates :name, presence: true, on: :update
   validates :location, presence: true, on: :update
-
   # regular expression to check valid australian phone numbers
   # The regex does not match with empty string hence no need for presence validation
   validates :phone,
@@ -11,4 +10,6 @@ class Profile < ApplicationRecord
               with: /\A\({0,1}((0|\+61)(2|4|3|7|8)){0,1}\){0,1}(\ |-){0,1}[0-9]{2}(\ |-){0,1}[0-9]{2}(\ |-){0,1}[0-9]{1}(\ |-){0,1}[0-9]{3}\z/,
               message: "Please enter a valid Australian mobile or landline number."
             }, on: :update
+
+  has_one_attached :picture
 end
