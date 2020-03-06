9e0587c7b994698f66d69cf5ebc57a1560c13f65
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-03-06 16:49:15 +1100

    restored seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,36 +11,36 @@ images = {
     accessory: ["accessory_1.jpg","accessory_2.jpg","accessory_3.jpg"]
 }
 
-# users = [
-#     "abc@email.com",
-#     "pqr@email.com",
-#     "xyz@email.com"
-# ];
+users = [
+    "abc@email.com",
+    "pqr@email.com",
+    "xyz@email.com"
+];
 
-# for i in 1..15
-#     users << Faker::Internet.email
-# end
+for i in 1..15
+    users << Faker::Internet.email
+end
 
-# for u in users
-#     user = User.create(email: u, password: "123456")
-#     p "created user #{user.id}"
-#     profile = user.create_profile(name: u.split("@")[0], phone: "0466123456", location: "sydney")
-#     p "created profile #{profile.id}"
-#     image_path = "#{Rails.root.join('db/seed_images/')}profile_#{Faker::Number.within(range: 1..4)}.jpg"
-#     filename = "#{profile.name.sub(" ", "_")}.jpg"
-#     profile.picture.attach(io: File.open(image_path), filename: filename, content_type: "image/jpg")
-#     p "attached image #{image_path} for #{profile.id}"
-# end
+for u in users
+    user = User.create(email: u, password: "123456")
+    p "created user #{user.id}"
+    profile = user.create_profile(name: u.split("@")[0], phone: "0466123456", location: "sydney")
+    p "created profile #{profile.id}"
+    image_path = "#{Rails.root.join('db/seed_images/')}profile_#{Faker::Number.within(range: 1..4)}.jpg"
+    filename = "#{profile.name.sub(" ", "_")}.jpg"
+    profile.picture.attach(io: File.open(image_path), filename: filename, content_type: "image/jpg")
+    p "attached image #{image_path} for #{profile.id}"
+end
 
-for i in 1..48
+for i in 1..50
     listing = Listing.create(
         title: Faker::Lorem.sentence(word_count: 10),
-        description: Faker::Lorem.paragraphs(number: 3),
+        description: Faker::Lorem.paragraphs(number: 3).join " ",
         brand: Faker::Lorem.word,
         model: Faker::Lorem.word,
         rate: Faker::Number.within(range: 1..50),
         category: Faker::Number.within(range: 0..2),
-        profile_id: Faker::Number.within(range: 1..18),
+        profile_id: Faker::Number.within(range: 1..users.length),
     )
     p "created listing #{listing.id}"
     image_files = images[listing.category.to_sym]
