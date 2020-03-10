b25dbf4c9471fa85bc4a812bd3df9dc82c5597b7
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-03-10 15:11:25 +1100

    added all the routes for requests

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,13 @@ Rails.application.routes.draw do
   get '/userprofile/:id', to: 'profiles#public', as: 'userprofile_public'
 
   get '/listings/my', to: 'listings#my', as: 'my_listings'
-  resources :listings
+  resources :listings, except: [:index]
   delete '/listings/:id/image/:index', to: 'listings#destroy_image', as: 'delete_listing_image'
+
+  resources :lease_requests, except: [:new, :index]
+  get '/lease_requests/new/:listing_id', to: 'lease_requests#new', as: 'new_lease_request'
+  get '/lease_requests/sent', to: 'lease_requests#sent', as: 'sent_lease_requests'
+  get '/lease_requests/received', to: 'lease_requests#received', as: 'received_lease_requests'
+  patch '/lease_requests/:id/accept', to: 'lease_requests#accept', as: 'accept_lease_request'
+  patch '/lease_requests/:id/reject', to: 'lease_requests#reject', as: 'reject_lease_request'
 end
