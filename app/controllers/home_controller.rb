ff1978c56abdbfa39e63208ef558bfd828fe71ca
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-03-12 10:45:28 +1100

    added logic for fetching listings in index view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,15 @@ class HomeController < ApplicationController
   def index
     if user_signed_in?
       if helpers.current_user_profile_updated?
-        @listings = Listing.all.where.not(profile_id: current_user.profile.id)
+        @listings = Listing.active.where.not(profile_id: current_user.profile.id).order(updated_at: :desc)
       else
         redirect_to edit_myprofile_path, alert: "Please update your details before viewing listings."
       end
     else
-      @listings = Listing.all
+      @listings = Listing.active.order(updated_at: :desc)
     end
+  end
+
+  def not_found    
   end
 end
