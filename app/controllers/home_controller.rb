84ee3f282591914b902ea4002e3f7618272630ad
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-03-11 23:39:42 +1100

    added logic to fetch listings

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
   def index
-    # render plain: "Welcome"
+    if user_signed_in?
+      if helpers.current_user_profile_updated?
+        @listings = Listing.all.where.not(profile_id: current_user.profile.id)
+      else
+        redirect_to edit_myprofile_path, alert: "Please update your details before viewing listings."
+      end
+    else
+      @listings = Listing.all
+    end
   end
 end
